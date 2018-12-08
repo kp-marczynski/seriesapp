c3836b912ab0f5f880075dded532ee1ad6bfb616
Marutan <martaf@cyberia.pl>
2018-12-08 22:07:56 +0100

    PO-11 delete fix

--- src/main/java/pl/marczynski/seriesapp/domain/FollowedSeries.java
+++ src/main/java/pl/marczynski/seriesapp/domain/FollowedSeries.java
@@ -32,7 +32,6 @@ public class FollowedSeries implements Serializable {
     private String comment;
 
     @ManyToOne(optional = false)
-    @JoinColumn(name = "user_id", referencedColumnName = "id")
     @NotNull
     @JsonIgnoreProperties("")
     private User user;
